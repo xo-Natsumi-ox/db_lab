0ae2e9c2548600b6277bf2d22f5e0e2581ab4635
xo-Natsumi-ox <53785210+xo-Natsumi-ox@users.noreply.github.com>
2021-01-06 18:16:37 +0200

    added lab6

--- src/main/java/com/shevchuk/service/GeneralService.java
+++ src/main/java/com/shevchuk/service/GeneralService.java
@@ -1,19 +1,68 @@
 package com.shevchuk.service;
 
-import com.shevchuk.dao.GeneralDao;
-
+import com.shevchuk.domain.GeneralEntity;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GeneralService<T, ID> {
-    List<T> findAll();
+public abstract class GeneralService<T extends GeneralEntity> implements ServiceInterface<T> {
 
-    T findById(ID id);
+  @Override
+  public abstract JpaRepository<T, Integer> getRepository();
 
-    void create(T entity);
+  @Override
+  public List<T> findAll() {
+    List<T> entityList = new LinkedList<>(getRepository().findAll());
+    if (entityList.isEmpty()) {
+      return null;
+    }
+    return entityList;
+  }
 
-    void update(T entity);
+  @Override
+  public T find(Integer id) {
+    Optional<T> optional = getRepository().findById(id);
+    if (optional.isPresent()) {
+      return optional.get();
+    } else {
+      return null;
+    }
+  }
 
-    void delete(ID id);
+  @Override
+  @Transactional
+  public T create(T entity) {
+    if (entity != null) {
+      return getRepository().save(entity);
+    }
+    return null;
+  }
 
-    GeneralDao<T, ID> getDao();
+  @Override
+  @Transactional
+  public T update(T entity) {
+    if (entity != null) {
+      Optional<T> optional = getRepository().findById(entity.getId());
+      if (optional.isPresent()) {
+        return getRepository().save(entity);
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
+
+  @Override
+  @Transactional
+  public T delete(Integer id) {
+    Optional<T> optional = getRepository().findById(id);
+    if (optional.isPresent()) {
+      getRepository().deleteById(id);
+      return optional.get();
+    } else {
+      return null;
+    }
+  }
 }
